8a2fab25089f3bfe938423667f3e9138e05faf59
Sandesh Subedi <subedisandesh0123@gmail.com>
2023-09-29 18:00:05 +0545

    minor changes

--- api/connection/config.php
+++ api/connection/config.php
@@ -8,7 +8,7 @@
 $HOST = 'https://auth-db1001.hstgr.io/';
 $USER = "u173237549_social";
 $PASSWORD = 'Onepiece@4321';
-$DB = "social";
+$DB = "u173237549_social";
 
 $con = mysqli_connect($HOST, $USER, $PASSWORD, $DB);
 
