daaa6cfc4529761c6ef03b3c75a491e58145ec8e
Sandesh Subedi <subedisandesh0123@gmail.com>
2023-10-08 17:52:34 +0545

    uploadImage

---
+++ api/functions/other/uploadImage.php
@@ -0,0 +1,41 @@
+<?php
+include "../../connection/config.php";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $id = $_POST['id'];
+    $location = $_POST['location'];
+    $currentImage = $_POST['currentImage'];
+
+    $image = $_FILES['file'];
+    $image_name = $image['name'];
+    $image_ext = $image['type'];
+    $image_tmp = $image['tmp_name'];
+    $image_location = "../../images/user/" . $location . '/' . $image_name;
+    move_uploaded_file($image_tmp, $image_location);
+    $image_location_db = "api/images/user/" . $location . '/' . $image_name;
+
+    if ($location === "profile") {
+        $sql = "UPDATE users SET profilePic='$image_location_db' where id='$id'";
+    } else if ($location === "cover") {
+        $sql = "UPDATE users SET coverPic='$image_location_db' where id='$id'";
+    }
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        if ($currentImage) {
+            unlink('../../images/user/' . $location . '/' . substr($currentImage, strrpos($currentImage, '/') + 1));
+        }
+        echo json_encode([
+            "status" => 200,
+            "message" => "Image updated succesfully",
+            "location" => $image_location_db
+        ]);
+    } else {
+        echo json_encode([
+            "status" => 500,
+            "message" => "Some error occured"
+        ]);
+    }
+}
+
+?>
