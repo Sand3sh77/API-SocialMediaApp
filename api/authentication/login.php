220a51833fdafed98c0c4c0b4d3375acfd6e3c08
Sandesh Subedi <subedisandesh0123@gmail.com>
2023-10-14 13:07:48 +0545

    Google Sign In

--- api/authentication/login.php
+++ api/authentication/login.php
@@ -1,62 +1,79 @@
 <?php
 include "../connection/config.php";
 
-if($_SERVER['REQUEST_METHOD']==='POST'){    
-    $password=$_POST['password'];
-    $username=$_POST['username'];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-    if($username!=='' and $password!==''){
+    if ($email !== '' and $password !== '') {
 
-        $sql="SELECT * FROM users WHERE username='$username'";
-        $result=mysqli_query($con,$sql);
-        $num=mysqli_num_rows($result);
-        if($num>0){
-            $user=mysqli_fetch_array($result);
-            $isValid=password_verify($password,$user['password']);
-            if($isValid){
+        $sql = "SELECT * FROM users WHERE email='$email'";
+        $result = mysqli_query($con, $sql);
+        $num = mysqli_num_rows($result);
+        if ($num > 0) {
+            $user = mysqli_fetch_array($result);
+            if ($user['method'] === "normal") {
+                $isValid = password_verify($password, $user['password']);
+                if ($isValid) {
 
-                function generateUniqueToken($length = 32) {
+                    function generateUniqueToken($length = 32)
+                    {
+                        return bin2hex(random_bytes($length));
+                    }
+
+                    $id = $user['id'];
+                    $token = generateUniqueToken();
+                    $sql = "INSERT INTO user_tokens(token,user_id) VALUES ('$token','$id')";
+                    $result = mysqli_query($con, $sql);
+
+                    echo json_encode(
+                        [
+                            "status" => 200,
+                            "token" => $token,
+                            "message" => "Login Successful.",
+                        ]
+                    );
+                } else {
+                    echo json_encode(
+                        [
+                            "status" => 550,
+                            "message" => "Wrong Password."
+                        ]
+                    );
+                }
+            } else {
+                function generateUniqueToken($length = 32)
+                {
                     return bin2hex(random_bytes($length));
                 }
-                
-                $id=$user['id'];
+                $id = $user['id'];
                 $token = generateUniqueToken();
-                $sql="INSERT INTO user_tokens(token,user_id) VALUES ('$token','$id')";
-                $result=mysqli_query($con,$sql);
-                
-            echo json_encode(
-                [
-                    "status"=>200,
-                    "token"=>$token,
-                    "message"=>"Login Successful.",
-                ]
-                );
-            }
-            else{
+                $sql = "INSERT INTO user_tokens(token,user_id) VALUES ('$token','$id')";
+                $result = mysqli_query($con, $sql);
+
                 echo json_encode(
                     [
-                        "status"=>550,
-                        "message"=>"Wrong Password."
+                        "status" => 200,
+                        "token" => $token,
+                        "message" => "Login by email successful.",
                     ]
-                    );
+                );
             }
-        }
-        else{
+        } else {
             echo json_encode(
                 [
-                    "status"=>500,
-                    "message"=>"User doesn't exist",
+                    "status" => 500,
+                    "message" => "User doesn't exist",
                 ]
-                );
+            );
         }
-    }
-    else{
+    } else {
         echo json_encode(
             [
-                "status"=>"505",
-                "message"=>"Fill all the fields",
+                "status" => "505",
+                "message" => "Fill all the fields",
             ]
-            );
+        );
     }
 }
 ?>
--- api/authentication/signup.php
+++ api/authentication/signup.php
@@ -1,46 +1,70 @@
 <?php
 include "../connection/config.php";
 
-if($_SERVER['REQUEST_METHOD']==='POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $email = $_POST['email'];
-    $password = $_POST['password'];
     $name = $_POST['name'];
-    // $city=$_POST['city'];
-    // $website=$_POST['website'];
+    $method = $_POST['method'];
+    $password = $_POST['password'];
+    $profilePic = $_POST['profilePic'];
 
-    if($username!=='' and $password!=='' and $email!=='' and $name!==""){
-        $sql="SELECT * FROM users WHERE username='$username' or email='$email'";
-        $result=mysqli_query($con,$sql);
+    if ($username !== '' and $password !== '' and $email !== '' and $name !== "") {
 
-        if($result){
-            $num=mysqli_num_rows($result);
-            if($num>0){
-                echo json_encode([
-                    "status"=>500,
-                    "message"=>"User already exists"
-                ]
-            );
-            }
-            else{
-                $hased_password=password_hash($password,PASSWORD_DEFAULT);
-                $sql="INSERT INTO users(username,email,password,name) VALUES('$username','$email','$hased_password','$name')";
-                $result=mysqli_query($con,$sql);
-                if($result){
-                    echo json_encode(array("status"=>200,
-                    "message"=>"Signup Successful.")                        
+        $sql = "SELECT * FROM users WHERE email='$email'";
+        $result = mysqli_query($con, $sql);
+
+        if ($result) {
+            $num = mysqli_num_rows($result);
+            $row = mysqli_fetch_assoc($result);
+            if ($num > 0) {
+                if ($row['method'] === 'normal') {
+                    echo json_encode(
+                        [
+                            "status" => 500,
+                            "message" => "Email already registered"
+                        ]
                     );
+                } else {
+                    echo json_encode([
+                        "status" => 200,
+                        "message" => "Redirect to login"
+                    ]);
+                }
+            } else {
+                if ($method === 'normal') {
+                    $hased_password = password_hash($password, PASSWORD_DEFAULT);
+                    $sql = "INSERT INTO users(username,email,password,name,method) VALUES('$username','$email','$hased_password','$name','normal')";
+                    $result = mysqli_query($con, $sql);
+                    if ($result) {
+                        echo json_encode(
+                            array(
+                                "status" => 200,
+                                "message" => "Signup Successful."
+                            )
+                        );
+                    }
+                } else {
+                    $sql = "INSERT INTO users(username,email,name,method,profilePic) VALUES('$username','$email','$name','email','$profilePic')";
+                    $result = mysqli_query($con, $sql);
+                    if ($result) {
+                        echo json_encode(
+                            array(
+                                "status" => 200,
+                                "message" => "Signup by email Successful."
+                            )
+                        );
+                    }
                 }
             }
         }
-    }
-    else{
+    } else {
         echo json_encode(
             [
-                "status"=>"505",
-                "message"=>"Fill all the fields",
+                "status" => "505",
+                "message" => "Fill all the fields",
             ]
-            );
+        );
     }
 
 }
