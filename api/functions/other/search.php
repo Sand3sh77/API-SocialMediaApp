12e3dc9d8642474d1e8538be87986081526413d2
Sandesh Subedi <subedisandesh0123@gmail.com>
2023-10-14 10:31:01 +0545

    cors

--- api/functions/other/search.php
+++ api/functions/other/search.php
@@ -1,6 +1,7 @@
 <?php
 include "../../connection/config.php";
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 
 if ($_SERVER["REQUEST_METHOD"] === 'POST') {
     $id = $_POST['id'];
