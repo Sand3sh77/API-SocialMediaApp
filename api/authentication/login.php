21e1543a8ecbc4b00d08c5df5dd31d684abc3f19
Sandesh Subedi <subedisandesh0123@gmail.com>
2023-10-14 13:22:56 +0545

    Login google bug fixed

--- api/authentication/login.php
+++ api/authentication/login.php
@@ -4,6 +4,7 @@ include "../connection/config.php";
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $method = $_POST['method'];
 
     if ($email !== '' and $password !== '') {
 
@@ -12,32 +13,41 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $num = mysqli_num_rows($result);
         if ($num > 0) {
             $user = mysqli_fetch_array($result);
-            if ($user['method'] === "normal") {
-                $isValid = password_verify($password, $user['password']);
-                if ($isValid) {
+            if ($method === "normal") {
+                if ($user['method'] === "normal") {
+                    $isValid = password_verify($password, $user['password']);
+                    if ($isValid) {
 
-                    function generateUniqueToken($length = 32)
-                    {
-                        return bin2hex(random_bytes($length));
+                        function generateUniqueToken($length = 32)
+                        {
+                            return bin2hex(random_bytes($length));
+                        }
+
+                        $id = $user['id'];
+                        $token = generateUniqueToken();
+                        $sql = "INSERT INTO user_tokens(token,user_id) VALUES ('$token','$id')";
+                        $result = mysqli_query($con, $sql);
+
+                        echo json_encode(
+                            [
+                                "status" => 200,
+                                "token" => $token,
+                                "message" => "Login Successful.",
+                            ]
+                        );
+                    } else {
+                        echo json_encode(
+                            [
+                                "status" => 550,
+                                "message" => "Wrong Password."
+                            ]
+                        );
                     }
-
-                    $id = $user['id'];
-                    $token = generateUniqueToken();
-                    $sql = "INSERT INTO user_tokens(token,user_id) VALUES ('$token','$id')";
-                    $result = mysqli_query($con, $sql);
-
-                    echo json_encode(
-                        [
-                            "status" => 200,
-                            "token" => $token,
-                            "message" => "Login Successful.",
-                        ]
-                    );
                 } else {
                     echo json_encode(
                         [
                             "status" => 550,
-                            "message" => "Wrong Password."
+                            "message" => "Please sign in with google"
                         ]
                     );
                 }
